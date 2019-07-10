ea88f2eb2223ef4e12beb7d1622b3f45ecc7aa5a
DanTayar <dantayar28@gmail.com>
2019-07-10 14:23:05 +0300

    navbar style

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
+import Navbar from './Navbar';
+
+import Home from './Home';
+import Menu from './Menu';
+import Contact from './Contact';
+
+const Landing =() => (<div className='Page'></div>)
+
+
+
+ class App extends React.Component{
+  render (){
+    return (
+      <Router>
+      <div>
+      <Route exact path='/home' component={Landing} />
+      <Navbar />
+      <Switch>
+        <Route exact path='/home' component={Home} />
+        <Route exact path='/menu' component={Menu} />
+        <Route exact path='/contact' component={Contact} />
+        <Redirect from = '/' to='/home' />
+
+
+      </Switch>
+      <footer />
+      </div>
+        
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Navbar.css';
+
+import { Link } from 'react-router-dom';
+
+const Navbar = ()=> (
+	<div className='Navbar'>
+	<h1> Meat War </h1>
+	<ul>
+		<li>
+		<a>Menu base </a>
+		</li>
+		<li>
+			<Link to='/home'>Home</Link>
+		</li>
+		<li>
+			<Link to='/menu'>Menu</Link>
+		</li>
+		<li>
+			<Link to='/contact'>Contact us</Link>
+		</li>
+	</ul>
+
+	</div>
+
+	);
+
+export default Navbar;

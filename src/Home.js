2b27a0f255ee60f860ddf602a297eca661f7fdc9
DanTayar <dantayar28@gmail.com>
2019-07-14 15:41:37 +0300

    front end

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import './Home.css';
+import LazyHero from 'react-lazy-hero';
 
 const Home = ()=> (
-	<div className='Page'>
-	Home
+	<div className='Home Page'>
+	 <LazyHero imageSrc='https://images6.alphacoders.com/600/thumb-350-600758.jpg'
+	 			opacity={0.3}
+	 			parallaxOffset={200}>
+
+
+	   <h1>Come eat the best meat in town  </h1>
+	   </LazyHero>
 	</div>
 	);
 
